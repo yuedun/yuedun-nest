a2ba2a481a0b3d6f06c4ae131ec3d4072cbcca98
yuedun <light_v@163.com>
2018-11-15 18:28:45 +0800

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Get, Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  root(): string {
+    return this.appService.root();
+  }
+  @Get()
+  index(): string {
+    return this.appService.root();
+  }
+}
